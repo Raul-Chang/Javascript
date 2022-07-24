8da2aa7cfd53853082b1798c9eb2fefeb0ba55a3
Raul Chang <4toulquiorraschiffer@gmail.com>
2022-07-24 19:35:18 -0300

    Primera Entrega Proyecto Final

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
-// Promedio de notas
+const alumnos = [
+    {
+        "nombre": "Raul Chang",
+        "notaMatematica": 10,
+        "notaFisica": 9,
+        "notaQuimica": 10,
+        "notaBiologia": 10,
+        "notaHistoria": 9
+    }, {
+        "nombre": "Luis Flores",
+        "notaMatematica": 7,
+        "notaFisica": 6,
+        "notaQuimica": 8,
+        "notaBiologia": 5,
+        "notaHistoria": 6
+    }, {
+        "nombre": "Jorge Peña",
+        "notaMatematica": 8,
+        "notaFisica": 7,
+        "notaQuimica": 6,
+        "notaBiologia": 10,
+        "notaHistoria": 8
+    },
+]
 
-// 5 materias, valores entre 0 y 10
-
-let sumaNotas = 0
-let nota = 0
-let arrayNotas = []
 let materias = ["Matematica", "Fisica" , "Quimica", "Biologia", "Historia"]
+let sumaNotas = 0
+let alumno
+let alumnoLog
 
-let alumno = prompt("Ingrese Nombre y Apellido del alumno")
-
-alert("Por favor ingrese el valor de las 5 notas del alumno, solo valores entre 1 y 10")
-
-
-function solicitarNotas(){
-    let notaIngresada = prompt(`Ingrese la nota de ${materias[i]} :`)
-    if (notaIngresada>=0 && notaIngresada<=10){
-        nota = notaIngresada
-        arrayNotas.push(nota)
-    } else {        
-        nota = 11               
-    }
+function solicitarAlumno(){
+    return prompt("Para ver las notas finales, ingrese el nombre de uno de los alumnos: Raul Chang, Luis Flores o Jorge Peña");
 }
 
-for(i=0; i<5; i++){
-    if(i<5){
-        solicitarNotas(); 
-        if(nota<=0 || nota>10){
-            alert(`Valor ingresado invalido. Intente nuevamente`)
-            i--
-        } else {
-            sumaNotas = Number(sumaNotas) + Number(nota)      
-            }
-    } 
-}       
-
-for( i=0 ; i<arrayNotas.length ; i++ ){
-    console.log(`Nota ingresada de ${materias[i]} es : ${arrayNotas[i]}`)
+function comparacionAlumno(){
+    alumno = solicitarAlumno()
+    alumnoLog = alumnos.find((alu) => alu.nombre === alumno)
 }
+
+comparacionAlumno()
+
+while(!alumnoLog) {
+    alert("Nombre Ingresado Invalido. Intente de nuevo");    
+    comparacionAlumno()        
+} 
+
+console.log(`Notas y promedio del alumno ${alumno}:`)
+
+materias.forEach( mat => {
+    console.log(`Nota de ${mat} es : ${alumnoLog[`nota${mat}`]}`)
+    sumaNotas += alumnoLog[`nota${mat}`]
+} )
 
 let promedio = sumaNotas / 5;
+
 console.log(`Promedio de notas del alumno ${alumno} en las 5 materias es = ${promedio}`)
-
-
-
 
 
